738ee04e175a08bc4a613a3cbe6de89fc1cd641d
MiyamotoTa <miyamoto_ta@zoho.com>
2017-09-18 03:28:28 +0900

    Remove MyBatis test code

--- dlnv/src/main/kotlin/takahiro/miyamoto/dlnv/DlnvApplication.kt
+++ dlnv/src/main/kotlin/takahiro/miyamoto/dlnv/DlnvApplication.kt
@@ -2,18 +2,9 @@ package takahiro.miyamoto.dlnv
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import takahiro.miyamoto.dlnv.infrastructure.persistence.PublicSafetyCommissionMapper
 
 @SpringBootApplication
-@RestController
-class DlnvApplication(
-        val publicSafetyCommisionMapper: PublicSafetyCommissionMapper
-) {
-    @RequestMapping("/")
-    fun hello() = publicSafetyCommisionMapper.findById(40)
-}
+class DlnvApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(DlnvApplication::class.java, *args)
