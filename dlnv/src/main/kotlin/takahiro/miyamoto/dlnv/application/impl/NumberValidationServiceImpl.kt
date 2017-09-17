83de8c6c3f17a31c629839ea219488bca386dfac
MiyamotoTa <miyamoto_ta@zoho.com>
2017-09-17 17:21:29 +0900

    Change parameters

--- dlnv/src/main/kotlin/takahiro/miyamoto/dlnv/application/impl/NumberValidationServiceImpl.kt
+++ dlnv/src/main/kotlin/takahiro/miyamoto/dlnv/application/impl/NumberValidationServiceImpl.kt
@@ -2,13 +2,24 @@ package takahiro.miyamoto.dlnv.application.impl
 
 import takahiro.miyamoto.dlnv.application.NumberValidationService
 import takahiro.miyamoto.dlnv.domain.model.license.LicenseNumber
+import takahiro.miyamoto.dlnv.infrastructure.persistence.PublicSafetyCommissionMapper
 
 /**
  * @author miyamoto_ta
  */
-class NumberValidationServiceImpl : NumberValidationService {
-    override fun validate(licenseNumber: LicenseNumber): Boolean {
-        TODO("実装する")
+class NumberValidationServiceImpl(
+        private val publicSafetyCommissionMapper: PublicSafetyCommissionMapper
+) : NumberValidationService {
+    override fun validate(publicSafetyCommissionId: Int,
+                          licenseAcquisitionYear: Int,
+                          controlNumber: Int,
+                          checkDigit: Int,
+                          reissuesNumber: Int): Boolean {
+        val publicSafetyCommission = publicSafetyCommissionMapper.findById(publicSafetyCommissionId)
+
+        val license = LicenseNumber(publicSafetyCommission, licenseAcquisitionYear, controlNumber, checkDigit, reissuesNumber)
+
+        return license.validateLicenseAcquisitionYear() && license.validateControlNumber() && license.validateCheckDigit() && license.validateReissuesNumber()
     }
 
 }
