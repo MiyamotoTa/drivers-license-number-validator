04e53b815aba168b68560ca335b2e8abe73c08f1
MiyamotoTa <miyamoto_ta@zoho.com>
2017-09-17 16:03:23 +0900

    Delegate to model

--- dlnv/src/main/kotlin/takahiro/miyamoto/dlnv/domain/model/license/LicenseNumber.kt
+++ dlnv/src/main/kotlin/takahiro/miyamoto/dlnv/domain/model/license/LicenseNumber.kt
@@ -16,4 +16,48 @@ data class LicenseNumber(
         val controlNumber: Int,
         val checkDigit: Int,
         val reissuesNumber: Int
-)
+) {
+    /**
+     * 免許証取得年が有効範囲内かどうかを検証する
+     *
+     * @return 免許証取得年が有効であればTrueを返す
+     */
+    fun validateLicenseAcquisitionYear(): Boolean = licenseAcquisitionYear in 0..99
+
+    /**
+     * 免許証管理番号が有効範囲内かどうかを検証する
+     *
+     * @return 免許証管理番号が有効であればTrueを返す
+     */
+    fun validateControlNumber() = controlNumber in 0..999999
+
+    /**
+     * チェックディジットが有効化どうかを検証する
+     *
+     * @return チェックディジットが正しければTrueを返す
+     */
+    fun validateCheckDigit(): Boolean {
+        val checkValue = publicSafetyCommission.id.format("%02d") + licenseAcquisitionYear.format("%02d") + controlNumber.format("%06d")
+
+        return checkDigit == calcCheckDigit(checkValue.toLong())
+    }
+
+    private fun Int.format(arg: String) = String.format(arg, this)
+
+    private fun calcCheckDigit(number: Long): Int {
+        val reversed = number.toString().reversed().toList().map {
+            it.toString().toInt()
+        }.toList()
+
+        val sum = reversed.withIndex().sumBy { it.value * (it.index % 6 + 2) }
+        val remainder = sum % 11
+
+        return if (remainder == 0 || remainder == 1) {
+            0
+        } else {
+            11 - remainder
+        }
+    }
+
+    fun validateReissuesNumber() = reissuesNumber in 0..9
+}
